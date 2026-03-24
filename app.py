aff3ba7a092d06fe134fdf827da2fe3e774ab762
Jorge <jorge.vieira0713@gmail.com>
2026-03-24 01:24:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 import streamlit as st
 import pandas as pd
+import os
 
-st.title("App Funcionando con pandas")
+st.title("App con CSV")
+
+@st.cache_data
+def cargar_datos():
+    ruta = os.path.join("data", "horse_limpio.csv")
+    df = pd.read_csv(ruta)
+    return df
+
+df = cargar_datos()
+
+st.write("Datos cargados:")
+st.write(df.head())
